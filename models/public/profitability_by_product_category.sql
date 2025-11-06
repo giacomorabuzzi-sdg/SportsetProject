41d81f16168a5ea8aaeafb66b8cb77d9d8e5f4ea
Giacomo Rabuzzi <giacomo.rabuzzi@sdggroup.com>
2025-11-06 16:39:16 +0100

    minor fix

--- models/public/profitability_by_product_category.sql
+++ models/public/profitability_by_product_category.sql
@@ -4,6 +4,7 @@ select
     sum(sales_vl) as total_revenue,
     sum(total_product_cost_vl) as total_costs,
     sum(order_qt) as order_quantity,
+    sum(sales_vl - total_product_cost_vl) as total_profit,
     (total_revenue - total_costs)/order_quantity as total_profit_per_piece
 
 from {{ ref('fct_orders')}} as ord
